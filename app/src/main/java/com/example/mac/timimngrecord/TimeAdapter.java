992204eb873962c219771eba64776195d7fa9f45
mac <x>
2018-03-28 16:43:22 +0200

    Construyendo llamadas a escritura en base de datos para mostrarlos en el recyclerView. Todavía no funciona.

---
+++ app/src/main/java/com/example/mac/timimngrecord/TimeAdapter.java
@@ -0,0 +1,82 @@
+package com.example.mac.timimngrecord;
+
+import android.content.DialogInterface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mac on 28/3/18.
+ */
+
+public class TimeAdapter extends RecyclerView.Adapter<TimeAdapter.ViewHolder>{
+
+    private ArrayList<Time> listTime;
+    private OnItemClickListener listener;
+
+    @Override
+    public  TimeAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.time_adapter, parent);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bind(position, listener);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listTime.size();
+    }
+
+    public interface OnItemClickListener{
+        void onItemClick(Time time);
+    }
+    public TimeAdapter(ArrayList<Time>listTime, OnItemClickListener listener){
+        this.listTime = listTime;
+        this.listener = listener;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txtTime;
+        private TextView txtDistance;
+        private TextView txtDelete;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            txtTime = itemView.findViewById(R.id.txtTime);
+            txtDistance = itemView.findViewById(R.id.txtDistance);
+            txtDelete = itemView.findViewById(R.id.txtDelete);
+        }
+
+        public void bind(final int position, final OnItemClickListener listener){
+            final Time time = listTime.get(position);
+            txtDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                                                // Este true es porque queremos escribir
+                    // llamamos a public Time(Context context, boolean write){
+                    // y por eso le pasamos, true , porque queremos escribir.
+                    new Time(itemView.getContext(), true).delete(time.getId());
+                    // El anterior time.getId() es para pasar el is del item
+                }
+            });
+
+            txtDistance.setText(time.getDistance());
+            txtTime.setText("en " + time.getTime());
+
+        }
+
+    }
+
+}
